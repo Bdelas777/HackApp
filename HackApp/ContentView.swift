d18a5c36d7beabd4be37f20b069788c8a42175df
Sebastián Presno <sebastiandash@icloud.com>
2024-04-19 11:33:05 -0600

    Fixed compatibility with portrait and landscape mode. Used Geo reader

--- HackApp/ContentView.swift
+++ HackApp/ContentView.swift
@@ -28,72 +28,74 @@ struct ContentView: View {
     
     let dummyData = ["Elemento 1", "Elemento 2", "Elemento 3", "Elemento 4", "Elemento 5", "Elemento 6","Elemento 7", "Elemento 8", "Elemento 9" ]
     var body: some View {
-        HStack(spacing: 0) {
-            // Right Part
-            VStack {
-                List{
-                    ForEach(dummyData, id: \.self){data in
-                        Text(data)
-                            .font(.headline)
-                            .padding()
+        GeometryReader{ geo in
+            HStack(spacing: 0) {
+                // Right Part
+                VStack {
+                    List{
+                        ForEach(dummyData, id: \.self){data in
+                            Text(data)
+                                .font(.headline)
+                                .padding()
+                        }
+                        .padding()
+                    }
+                    .frame(width: geo.size.width / 2)
+                    //.background(Color.red)
+                }
+                Divider()
+                VStack {
+                    Chart(products) { product in
+                        SectorMark(
+                            angle: .value(
+                                Text(verbatim: product.title),
+                                product.revenue
+                            )
+                        )
+                        .foregroundStyle(
+                            by: .value(
+                                Text(verbatim: product.title),
+                                product.title
+                            )
+                        )
                     }
                     .padding()
-                }
-                    //.background(Color.red)
-            }
-            .frame(width: UIScreen.main.bounds.width / 2)
-            Divider()
-            VStack {
-                Chart(products) { product in
-                    SectorMark(
-                        angle: .value(
-                            Text(verbatim: product.title),
-                            product.revenue
-                        )
-                    )
-                    .foregroundStyle(
-                        by: .value(
-                            Text(verbatim: product.title),
-                            product.title
-                        )
-                    )
-                }
-                .padding()
-                Divider()
+                    Divider()
                     //.background(Color.blue)
-                ZStack{
-                    Circle()
-                        .stroke(lineWidth: 20)
-                        .opacity(0.3)
-                        .padding()
-                    Circle()
-                        .trim(from: 0, to: CGFloat(1-(timeRemaining/10)))
-                        .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
-                        .rotationEffect(.degrees(-90))
-                        .padding()
-                    Text(formattedTime())
-                        .font(.largeTitle)
-                        .bold()
-                }
+                    ZStack{
+                        Circle()
+                            .stroke(lineWidth: 20)
+                            .opacity(0.3)
+                            .padding()
+                        Circle()
+                            .trim(from: 0, to: CGFloat(1-(timeRemaining/10)))
+                            .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
+                            .rotationEffect(.degrees(-90))
+                            .padding()
+                        Text(formattedTime())
+                            .font(.largeTitle)
+                            .bold()
+                    }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     //.background(Color.green)
-                HStack{
-                    Button{
-                        isRunning.toggle()
-                        if(isRunning){
-                            startTimer()
+                    HStack{
+                        Button{
+                            isRunning.toggle()
+                            if(isRunning){
+                                startTimer()
+                            }
+                            else{
+                                stopTimer()
+                            }
+                        }label: {
+                            Image(systemName: isRunning ? "stop.fill": "play.fill")
+                                .frame(width: 50, height: 50)
+                                .font(.largeTitle)
                         }
-                        else{
-                            stopTimer()
-                        }
-                    }label: {
-                        Image(systemName: isRunning ? "stop.fill": "play.fill")
-                            .frame(width: 50, height: 50)
-                            .font(.largeTitle)
                     }
                 }
+                .frame(width: geo.size.width / 2)
             }
-            .frame(width: UIScreen.main.bounds.width / 2)
         }
     }
     private func formattedTime() -> String{
--- HackApp/HomeViewAdmin.swift
+++ HackApp/HomeViewAdmin.swift
@@ -10,23 +10,33 @@ import SwiftUI
 struct HomeViewAdmin: View {
     let dummyData = ["Elemento 1", "Elemento 2", "Elemento 3", "Elemento 4", "Elemento 5", "Elemento 6","Elemento 7", "Elemento 8", "Elemento 9" ]
     var body: some View {
-        ZStack{
-            List{
-                ForEach(dummyData, id: \.self){data in
-                    Text(data)
-                        .font(.title3)
-                        .padding()
+        NavigationStack{
+            GeometryReader{ geo in
+                ZStack{
+                    List{
+                        ForEach(dummyData, id: \.self){data in
+                            NavigationLink(destination: ContentView()){
+                                Text(data)
+                                    .font(.title)
+                                    .fontWeight(.medium)
+                                    .padding()
+                            }
+                        }
+                    }
+                    .listRowSpacing(10)
+                    Button{
+                        
+                    }label: {
+                        Label("Nuevo Hackathon", systemImage: "plus")
+                            .font(.title)
+                            .bold()
+                            .padding()
+                    }
+                    .buttonStyle(MainViewButtonStyle())
+                    .offset(x: geo.size.width / 3.8, y: geo.size.height / 2.5)
                 }
             }
-            Button{
-                
-            }label: {
-                Label("Nuevo Hackathon", systemImage: "plus")
-                    .font(.title)
-                    .bold()
-            }
-            .buttonStyle(MainViewButtonStyle())
-            .offset(x: 380, y: 280)
+            .navigationTitle("Tus Hackatons")
         }
     }
 }
