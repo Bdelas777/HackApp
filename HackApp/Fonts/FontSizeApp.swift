c58c76fd38adf7c99180276618eb35aab6fe1bfa
Bernardo de la Sierra Rábago <A01735821@tec.mx>
2024-09-05 17:04:50 -0600

    feat: add button component

---
+++ HackApp/Fonts/FontSizeApp.swift
@@ -0,0 +1,22 @@
+//
+//  FontSizeApp.swift
+//  HackApp
+//
+//  Created by Alumno on 05/09/24.
+//
+
+
+import Foundation
+
+enum FontSizeApp: CGFloat {
+    case largeTitle = 64
+    case title = 38
+    case subtitle = 34
+    case heading = 30
+    case largeButtonText = 48
+    case buttonText = 26
+    case label = 24
+    case bigText = 22
+    case text = 20
+    case captions = 18
+}
---
+++ HackApp/Views/Components/Buttons/StartButton.swift
@@ -0,0 +1,50 @@
+//
+//  StartButton.swift
+//  HackApp
+//
+//  Created by Alumno on 05/09/24.
+//
+
+import SwiftUI
+
+struct StartButton: View {
+    var title: String
+    var iconName: String
+    var hint: String
+    var action: ()-> Void
+    
+    var body: some View {
+        Button(action: action){
+            ZStack{
+                VStack{
+                    Image(systemName: iconName)
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .frame(width: 70, height: 70)
+                    Text(title)
+                        .foregroundColor(.white)
+                        .font(.custom("Lato", size: FontSizeApp.largeButtonText.rawValue))
+                        .bold()
+                        .frame(width: 150)
+                }// End VStack
+                .frame(width: 400, height: 250)
+                .background(Color("LightBlue"))
+                .cornerRadius(30)
+                .padding(5)
+                .foregroundColor(.white)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .accessibilityLabel(title)
+                .accessibilityHint(hint)
+            }// End ZStack
+        }// End Button
+    }
+}
+
+#Preview {
+    StartButton(
+        title: "Button",
+        iconName: "envelope.fill",
+        hint: "Esto es un boton", action: {}
+    )
+}
