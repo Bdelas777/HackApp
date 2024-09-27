878a38ed7186cb1062cd81d5b9decd4c9eef96cc
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-09-27 17:08:45 -0600

    feat. new style judges

--- HackApp/Views/Judge/JudgesView.swift
+++ HackApp/Views/Judge/JudgesView.swift
@@ -7,50 +7,70 @@
 import SwiftUI
 
 struct JudgesView: View {
-    @State private var showModal = true // Show modal by default
+    @State private var showModal = false
     @State private var hackNameInput = ""
     @State private var selectedJudges: [String: [String: Int]]?
     @State private var errorMessage: String?
-
+    @State private var hasSearched = false
     @ObservedObject var viewModel = HacksViewModel()
 
     var body: some View {
         VStack {
-            Button(action: {
-                showModal.toggle()
-            }) {
-                Text("Buscar Jueces por Hack")
+            if let judges = selectedJudges {
+                // Mostrar la lista de jueces si hay jueces seleccionados
+                Text("Escoge tu nombre:")
+                    .font(.title)
+                    .padding()
+                List(judges.keys.sorted(), id: \.self) { judge in
+                    Text(judge)
+                        .font(.title2)
+                }
+            } else if hasSearched {
+                // Mostrar el mensaje de error si se ha buscado pero no hay jueces
+                Text(errorMessage ?? "No se encontro ese hack.")
+                    .foregroundColor(.red)
+                    .font(.title)
+                    .padding()
+            } else {
+                // Mensaje inicial antes de buscar
+                Text("Realiza una búsqueda del hackathon.")
+                    .font(.title)
+                    .padding()
             }
-            .sheet(isPresented: $showModal) {
-                VStack {
+
+            Spacer()
+            
+            Button {
+                showModal.toggle()
+            } label: {
+                Label("Buscar Hackathon", systemImage: "search")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                    .foregroundColor(.blue)
+            }
+            .popover(isPresented: $showModal) {
+                VStack(spacing: 20) {
                     TextField("Ingrese el nombre del Hack", text: $hackNameInput)
-                        .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
 
                     Button(action: {
                         fetchJudges()
+                        showModal = false
                     }) {
                         Text("Buscar Jueces")
+                            .fontWeight(.bold)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
                     }
                     .padding()
-                    
-                    if let errorMessage = errorMessage {
-                        Text(errorMessage)
-                            .foregroundColor(.red)
-                            .padding()
-                    } else if let judges = selectedJudges {
-                        Text("Jueces encontrados:")
-                            .padding()
-                        
-                        List(judges.keys.sorted(), id: \.self) { judge in
-                            Text(judge)
-                        }
-                    } else {
-                        Text("Esperando búsqueda...")
-                            .padding()
-                    }
                 }
                 .padding()
+                .frame(width: 300) // Ajusta el ancho del popover
             }
         }
         .onAppear {
@@ -62,12 +82,20 @@ struct JudgesView: View {
         viewModel.getJudges(for: hackNameInput.lowercased()) { result in
             switch result {
             case .success(let judges):
-                selectedJudges = judges
-                errorMessage = nil
-                print("Jueces encontrados: \(judges)")
-            case .failure(let error):
+                // Si se reciben jueces, asignarlos. Si el diccionario está vacío, tratarlo como error.
+                if judges.isEmpty {
+                    selectedJudges = nil
+                    errorMessage = "No se han encontrado jueces para el hack."
+                } else {
+                    selectedJudges = judges
+                    errorMessage = nil
+                }
+                hasSearched = true
+            case .failure:
+                // Manejar el error de la búsqueda de jueces
                 selectedJudges = nil
-                errorMessage = "Error al obtener los jueces: \(error.localizedDescription)"
+                errorMessage = "No se ha encontrado ese hack."
+                hasSearched = true
             }
         }
     }
@@ -78,4 +106,3 @@ struct JudgesView_Previews: PreviewProvider {
         JudgesView()
     }
 }
-
